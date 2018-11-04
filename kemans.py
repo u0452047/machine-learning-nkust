1d9ca7a9536b6784520e88d9d6573b29542fab4f
u0452047 <u0452047@nkfust.edu.tw>
2018-11-05 01:15:51 +0800

    第一次

---
+++ kemans.py
@@ -0,0 +1,16 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+from sklearn.cluster import KMeans
+
+x =np.random.rand(100,2)
+x[0]
+print(x)
+plt.scatter(x[:,0],x[:,1],s=20)
+plt.show()
+clf = KMeans(n_clusters=4)
+print(clf.fit(x))
+print(clf.labels_)
+print(x)
+plt.scatter(x[:,0],x[:,1],c=clf.labels_)
+plt.show()
+plt.show()
